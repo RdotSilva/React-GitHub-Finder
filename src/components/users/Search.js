6372b4f717083432c986eb20996f0785fcc7f0fb
Ryan Silva <RyandotSilva@gmail.com>
2019-06-10 20:16:05 -0400

    Add submit button to Search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,6 +6,11 @@ export class Search extends Component {
 			<div>
 				<form className="form">
 					<input type="text" name="text" placeholder="Search Users..." />
+					<input
+						type="submit"
+						value="Search"
+						className="btn btn-dark btn-block"
+					/>
 				</form>
 			</div>
 		);
