54cf8bf47dae099a2f438efa25fb0cf89ad6329c
Ryan Silva <RyandotSilva@gmail.com>
2019-06-11 15:57:17 -0400

    Add clear button to search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -35,6 +35,12 @@ export class Search extends Component {
 						className="btn btn-dark btn-block"
 					/>
 				</form>
+				<button
+					className="btn btn-light btn-block"
+					onClick={this.props.clearUsers}
+				>
+					Clear
+				</button>
 			</div>
 		);
 	}
