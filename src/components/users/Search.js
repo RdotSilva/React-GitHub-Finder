8f2caa24230eca6685ec1660b42ae7f1df75cfa5
Ryan Silva <RyandotSilva@gmail.com>
2019-06-11 16:03:58 -0400

    Refactor using destructuring

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -21,6 +21,7 @@ export class Search extends Component {
 	};
 
 	render() {
+		const { showClear, clearUsers } = this.props;
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className="form">
@@ -37,12 +38,11 @@ export class Search extends Component {
 						className="btn btn-dark btn-block"
 					/>
 				</form>
-				<button
-					className="btn btn-light btn-block"
-					onClick={this.props.clearUsers}
-				>
-					Clear
-				</button>
+				{showClear && (
+					<button className="btn btn-light btn-block" onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
