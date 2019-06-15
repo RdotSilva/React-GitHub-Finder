9a278399ac2858464c33e911a75996eeeb326003
Ryan Silva <RyandotSilva@gmail.com>
2019-06-14 21:09:17 -0400

    Add public repos

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -96,6 +96,9 @@ export class User extends Component {
 				<div className="card text-center">
 					<div className="badge badge-primary">Followers: {followers}</div>
 					<div className="badge badge-primary">Following: {following}</div>
+					<div className="badge badge-primary">
+						Public Repos: {public_repos}
+					</div>
 				</div>
 			</Fragment>
 		);
