b74c30717e1973b4672aa3149dd134aeab59f5c9
Ryan Silva <RyandotSilva@gmail.com>
2019-06-15 22:21:28 -0400

    Add getUserRepos method

--- src/App.js
+++ src/App.js
@@ -54,6 +54,19 @@ class App extends Component {
 		this.setState({ user: res.data, loading: false });
 	};
 
+	// Get users repos
+	getUserRepos = async username => {
+		this.setState({ loading: true });
+
+		const res = await Axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+				process.env.REACT_APP_GITHUB_CLIENT_ID
+			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ repos: res.data, loading: false });
+	};
+
 	// Clears users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
