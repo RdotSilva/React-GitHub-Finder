5b5c2eb3c6c2d7563530f68d1c50f482d0c7ae77
Ryan Silva <RyandotSilva@gmail.com>
2019-06-15 22:27:58 -0400

    Add prop types

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const RepoItem = ({ repo }) => {
 	return <div />;
+};
+
+RepoItem.propTypes = {
+	repo: PropTypes.object.isRequired
 };
 
 export default RepoItem;
