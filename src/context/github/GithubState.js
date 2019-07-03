f606de39f4f0bebee17fc27abdcbdbc12d2b0100
Ryan Silva <RyandotSilva@gmail.com>
2019-07-02 20:42:30 -0400

    Add getUser to provider

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -83,7 +83,8 @@ const GithubState = props => {
 				repos: state.repos,
 				loading: state.loading,
 				searchUsers,
-				clearUsers
+				clearUsers,
+				getUser
 			}}
 		>
 			{props.children}
