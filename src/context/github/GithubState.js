27b83b7ecc987a7d1333dec75cdcd66a8c3da4b0
Ryan Silva <RyandotSilva@gmail.com>
2019-07-03 18:26:40 -0400

    Fix axios typo

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -73,7 +73,7 @@ const GithubState = props => {
 		// setLoading(true);
 		setLoading();
 
-		const res = await Axios.get(
+		const res = await axios.get(
 			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
 				process.env.REACT_APP_GITHUB_CLIENT_ID
 			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
