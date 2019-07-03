bc8ad2793a95d3653ada822ac9485cbc0d516abf
Ryan Silva <RyandotSilva@gmail.com>
2019-07-02 20:06:22 -0400

    Add search users case

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -8,6 +8,11 @@ import {
 
 export default (state, action) => {
 	switch (action.type) {
+		case SEARCH_USERS:
+			return {
+				...state,
+				users: action.payload
+			};
 		case SET_LOADING:
 			return {
 				...state,
