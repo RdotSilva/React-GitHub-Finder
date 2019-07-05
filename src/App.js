02ab1a0e3bb3dfd321912d3ceb6951a226b80890
Ryan Silva <RyandotSilva@gmail.com>
2019-07-04 20:35:06 -0400

    Move Search and Users to Home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
+import Home from "./components/pages/Home";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
