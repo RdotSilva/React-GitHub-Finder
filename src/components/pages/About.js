7436043a863256e2d8bb92685933904541ea2547
Ryan Silva <RyandotSilva@gmail.com>
2019-09-02 15:05:59 -0400

    Update about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,11 @@ import React, { Fragment } from "react";
 const About = () => {
 	return (
 		<Fragment>
-			<h1>About This App</h1>
-			<p>App to search GitHub users</p>
+			<h1>About QuickHub</h1>
+			<p>
+				QuickHub allows you to search GitHub users and get the information you
+				need plain and simple.
+			</p>
 			<p>Version: 1.0.0</p>
 		</Fragment>
 	);
