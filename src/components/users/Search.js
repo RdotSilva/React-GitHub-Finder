0b196232ab2b08d3bad984ab144c05b21e8b891f
Ryan Silva <RyandotSilva@gmail.com>
2019-09-02 14:58:17 -0400

    Remove class based example code

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,38 +8,28 @@ const Search = () => {
 
 	const [text, setText] = useState("");
 
-	// class way
-	// const onChange = e => this.setState({ [e.target.name]: e.target.value });
-
-	// useState way
 	const onChange = e => setText(e.target.value);
 
 	const onSubmit = e => {
 		e.preventDefault();
-		// if (this.state.text === "") {
+
 		if (text === "") {
-			// this.props.setAlert("Please enter something", "light");
 			alertContext.setAlert("Please enter something", "light");
 		} else {
-			// this.props.searchUsers(this.state.text);
-			// searchUsers(text);
 			githubContext.searchUsers(text);
-			// this.setState({ text: "" });
+
 			setText("");
 		}
 	};
 
 	return (
 		<div>
-			{/* <form onSubmit={this.onSubmit} className="form"> */}
 			<form onSubmit={onSubmit} className="form">
 				<input
 					type="text"
 					name="text"
 					placeholder="Search Users..."
-					// value={this.state.text}
 					value={text}
-					// onChange={this.onChange}
 					onChange={onChange}
 				/>
 				<input
