301509eeda0dfefaa7d0280fd74d5336aa71aa1b
Ryan Silva <RyandotSilva@gmail.com>
2019-09-02 15:04:29 -0400

    Clean up user component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,6 @@
 import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import GithubContext from "../../context/github/githubContext";
 
@@ -71,7 +70,7 @@ const User = ({ match }) => {
 						<li>
 							{login && (
 								<Fragment>
-									<strong>Username:</strong> {login}
+									<strong>GitHub username:</strong> {login}
 								</Fragment>
 							)}
 						</li>
